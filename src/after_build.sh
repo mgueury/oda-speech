bb25ed642be8704d10fc3322fd8baaacb79bd07f
Marc Gueury <marc.gueury@oracle.com>
2025-11-05 15:53:19 +0100

    v3.0

---
+++ src/after_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export SRC_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export ROOT_DIR=${SRC_DIR%/*}
+cd $ROOT_DIR
+
+. ./starter.sh env
+
+title "INSTALLATION DONE"
+echo
+# echo "(experimental) Cohere with Tools and GenAI Agent:"
+# echo "http://${BASTION_IP}:8081/"
+# echo "-----------------------------------------------------------------------"
+
+echo "URLs" > $FILE_DONE
+append_done "-----------------------------------------------------------------------"
+append_done "OpenAI Gateway URLs:"
+append_done "- http://${BASTION_IP}/app/getToken"
+append_done "- https://${APIGW_HOSTNAME}/${TF_VAR_prefix}/app/getToken"
+append_done
